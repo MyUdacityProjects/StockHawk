ec1ff622c1482388cb1f1c4df5b2e59ab91c93c4
hkasera <hkasera5@gmail.com>
2016-04-25 22:24:40 +0530

    Added colors to colors.xml

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailStockActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailStockActivity.java
@@ -3,9 +3,9 @@ package com.sam_chordas.android.stockhawk.ui;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
@@ -24,7 +24,7 @@ import com.sam_chordas.android.stockhawk.data.QuoteProvider;
 public class DetailStockActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
 
-    public static final String TAG_STOCK_SYMBOL = "STOCK_SYMBOL";
+    public static final String STOCK_SYMBOL = "symbol";
     private static final int STOCKS_LOADER = 1;
 
     private String currency;
@@ -32,7 +32,7 @@ public class DetailStockActivity extends AppCompatActivity implements LoaderMana
 
     public static Intent getStartActivityIntent(Context context, String currency) {
         Intent intent = new Intent(context, DetailStockActivity.class);
-        intent.putExtra(DetailStockActivity.TAG_STOCK_SYMBOL, currency);
+        intent.putExtra(DetailStockActivity.STOCK_SYMBOL, currency);
 
         return intent;
     }
@@ -42,7 +42,7 @@ public class DetailStockActivity extends AppCompatActivity implements LoaderMana
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_line_graph);
 
-        currency = getIntent().getStringExtra("symbol");
+        currency = getIntent().getStringExtra(STOCK_SYMBOL);
 
         setTitle(currency);
         lineChartView = (LineChartView) findViewById(R.id.linechart);
@@ -86,9 +86,9 @@ public class DetailStockActivity extends AppCompatActivity implements LoaderMana
             maximumPrice = Math.max(maximumPrice, price);
         }
 
-        lineSet.setColor(Color.parseColor("#758cbb"))
-                .setFill(Color.parseColor("#2d374c"))
-                .setDotsColor(Color.parseColor("#758cbb"))
+        lineSet.setColor(ContextCompat.getColor(this, R.color.line_color))
+                .setFill(ContextCompat.getColor(this, R.color.line_fill))
+                .setDotsColor(ContextCompat.getColor(this, R.color.line_color))
                 .setThickness(4)
                 .setDashed(new float[]{10f, 10f});
 
@@ -96,7 +96,7 @@ public class DetailStockActivity extends AppCompatActivity implements LoaderMana
         lineChartView.setBorderSpacing(Tools.fromDpToPx(15))
                 .setYLabels(AxisController.LabelPosition.OUTSIDE)
                 .setXLabels(AxisController.LabelPosition.NONE)
-                .setLabelsColor(Color.parseColor("#6a84c3"))
+                .setLabelsColor(ContextCompat.getColor(this, R.color.line_labels))
                 .setXAxis(false)
                 .setYAxis(false)
                 .setAxisBorderValues(Math.round(Math.max(0f, minimumPrice - 5f)), Math.round(maximumPrice + 5f))
@@ -105,10 +105,11 @@ public class DetailStockActivity extends AppCompatActivity implements LoaderMana
 
         Animation anim = new Animation();
 
-        if (lineSet.size() > 1)
+        if (lineSet.size() > 1) {
             lineChartView.show(anim);
-        else
-            Toast.makeText(this, "No data", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, getString(R.string.no_stock_data), Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
